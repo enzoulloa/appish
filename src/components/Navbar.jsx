671b3ff6ce3cc99a3e9c76b61685ffbe53c2adb6
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-13 01:35:28 -0300

    fix: :bug: fix header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,19 @@ import logo from '../assets/icons/appishLogo.svg'
 
 const Navbar = () => {
   return (
-    <nav className=' fixed left-0 top-0 right-0 bottom-auto mt-0 z-50 pt-[15px]'>
-      <div className=' w-[90%] relative max-w-[1100px] mx-auto py-[10px] pl-5 flex flex-row justify-between items-center'>
-        <a className=' w-[65px] h-[30px] ' href='#appish'>
-          <img className=' align-middle' src={logo} alt='Appish' />
-        </a>
-        <div className=' flex flex-row justify-between items-center'>
-          <a className=' ml-[5px] py-[6px] px-[15px] rounded-full text-[13px] bg-[#226cff] text-white leading-[28px] cursor-pointer font-semibold ' href='https://www.linkedin.com/in/enzo-ulloa-2639a1194/' target='_blank' rel='noreferrer'>Download Now</a>
-          <div className=' px-[10px] py-[5px] text-[#000] text-2xl cursor-pointer'>
-            <span>
-              <svg stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 512 512' height='24px' width='24px' xmlns='http://www.w3.org/2000/svg'><path fill='none' stroke-linecap='round' stroke-miterlimit='10' stroke-width='48' d='M88 152h336M88 256h336M88 360h336' /></svg>
-            </span>
+    <nav className=' pt-[15px]'>
+      <div className=' w-[90%] max-w-[1100px] mx-auto '>
+        <div className=' py-[10px] pl-5 flex flex-row w-full max-w-none rounded-[73px] mx-auto bottom-auto justify-between items-center'>
+          <a className=' w-[65px] h-[30px] ' href='#appish'>
+            <img className='  object-contain h-full align-middle' src={logo} alt='Appish' />
+          </a>
+          <div className=' flex flex-row justify-between items-center'>
+            <a className=' ml-[5px] py-[6px] px-[15px] rounded-full text-[13px] bg-[#226cff] text-white leading-[28px] cursor-pointer font-semibold ' href='https://www.linkedin.com/in/enzo-ulloa-2639a1194/' target='_blank' rel='noreferrer'>Download Now</a>
+            <div className=' px-[10px] py-[5px] text-[#000] text-2xl cursor-pointer'>
+              <span>
+                <svg stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 512 512' height='24px' width='24px' xmlns='http://www.w3.org/2000/svg'><path fill='none' stroke-linecap='round' stroke-miterlimit='10' stroke-width='48' d='M88 152h336M88 256h336M88 360h336' /></svg>
+              </span>
+            </div>
           </div>
         </div>
       </div>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from '../components/Navbar'
 
 const Header = () => {
   return (
-    <header>
+    <header className=' fixed left-0 top-0 right-0 w-full bottom-auto z-50 bg-white'>
       <Navbar />
     </header>
   )
