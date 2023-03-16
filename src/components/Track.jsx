29db9e916fba5729e08ef3cb3df650103eceebc7
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-16 16:54:04 -0300

    feat: :iphone: add tablet responsive styles

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,9 +1,9 @@
 const Track = ({ image, title, paragraph }) => {
   return (
     <article className=' flex flex-col justify-center items-center'>
-      <img className='object-contain   align-middle' src={image} alt={title + ' icon'} />
-      <h3 className=' mt-3 text-[22px] leading-[31px] text-[#231656] font-bold'>{title}</h3>
-      <p className='mt-[6px] text-base text-[#6f6a80] '>{paragraph}</p>
+      <img className='object-contain align-middle' src={image} alt={title + ' icon'} />
+      <h3 className=' mt-3 text-[22px] leading-[31px] text-[#231656] font-bold md:mt-8'>{title}</h3>
+      <p className='mt-[6px] text-base text-[#6f6a80] md:mt-[17px] md:leading-[160%]'>{paragraph}</p>
     </article>
   )
 }
