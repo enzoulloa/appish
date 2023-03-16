a6fd7ce532ba1cd279f0ba7a11209225aa9ca0cd
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-16 16:30:26 -0300

    feat: :iphone: add tablet responsive styles

--- src/components/Phones.jsx
+++ src/components/Phones.jsx
@@ -7,12 +7,12 @@ import phone1 from '../assets/images/Group.png'
 const Phones = () => {
   return (
     <section className=' overflow-hidden '>
-      <div className=' pb-[50px]  w-[90%] pt-[80px] max-w-[1100px] mx-auto'>
+      <div className=' pb-[50px] w-[90%] pt-[80px] max-w-[1100px] mx-auto md:w-[85%] md:pt-[100px] '>
         <div className=' flex w-full items-center justify-center relative'>
-          <div className='absolute top-[6%] w-[160px] h-[160px] right-0 left-auto bottom-auto '>
+          <div className='absolute top-[6%] w-[160px] h-[160px] right-0 left-auto bottom-auto md:w-[270px] md:h-[270px] md:top-[5%]'>
             <img className='overflow-visible object-contain w-full h-full max-w-full align-middle' src={blue} alt='Blue circle' />
           </div>
-          <div className=' absolute w-[110px] h-[110px] left-[5%] top-auto right-auto bottom-[3%] '>
+          <div className=' absolute w-[110px] h-[110px] left-[5%] top-auto right-auto bottom-[3%] md:w-[200px] md:h-[200px]'>
             <img className='overflow-visible object-contain w-full h-full max-w-full align-middle' src={orange} alt='Orange circle' />
           </div>
           <div className=' -mr-[60px] -mb-[40px] relative w-full max-w-[300px] -rotate-[20deg]'>
