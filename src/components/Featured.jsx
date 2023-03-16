8baa2a3857ef06ae9a27f3baa227a79294575cbd
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-16 16:43:13 -0300

    feat: :iphone: add tablet responsive styles

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -5,13 +5,13 @@ import ipsun4 from '../assets/logos/ipsun4.png'
 
 const Featured = () => {
   return (
-    <section className=' pt-[80px] max-w-[900px] pb-[150px] flex flex-col justify-center gap-4 w-[90%] mx-auto items-center'>
+    <section className=' pt-[80px] max-w-[900px] pb-[150px] flex flex-col justify-center gap-4 w-[90%] mx-auto items-center md:pt-10 md:w-[85%]'>
       <p className=' text-sm leading-6 text-[#6f6a80]'>Trusted by top-rated-apps</p>
-      <div className=' w-[90%] grid grid-cols-2 gap-x-10 gap-y-[30px]  max-w-[370px] mt-[10px] '>
-        <img src={ipsun1} alt='' />
-        <img src={ipsun2} alt='' />
-        <img src={ipsun3} alt='' />
-        <img src={ipsun4} alt='' />
+      <div className=' w-[90%] grid grid-cols-2 gap-x-10 gap-y-[30px] max-w-[370px] mt-[10px] md:gap-x-[60px] md:w-full md:grid-cols-4 md:max-w-none md:gap-y-4 items-center'>
+        <img className='w-full' src={ipsun1} alt='Logo' />
+        <img className='w-full' src={ipsun2} alt='Logo' />
+        <img className='w-full' src={ipsun3} alt='Logo' />
+        <img className='w-full' src={ipsun4} alt='Logo' />
       </div>
     </section>
   )
