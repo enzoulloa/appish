425def372fcf08a7d413c7ffc9ee844dab7063ce
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-16 17:01:42 -0300

    feat: :iphone: add tablet responsive styles

--- src/components/Phone.jsx
+++ src/components/Phone.jsx
@@ -4,9 +4,9 @@ import image from '../assets/images/Device-1.png'
 
 const Phone = () => {
   return (
-    <article className=' w-[90%] mb-10 relative max-w-[475px] '>
-      <img className='w-[180px] h-[180px] absolute -left-[9%] top-0 right-auto bottom-auto overflow-hidden object-cover align-middle' src={blue} alt='Blue circle' />
-      <img className='w-[120px] h-[120px] absolute left-auto top-auto right-0 bottom-0 overflow-hidden object-cover align-middle' src={orange} alt='Orange circle' />
+    <article className=' w-[90%] mb-10 relative max-w-[475px] md:w-[50%] md:mb-10'>
+      <img className='w-[180px] h-[180px] absolute -left-[9%] top-0 right-auto bottom-auto overflow-hidden object-cover align-middle md:w-[270px] md:h-[270px]' src={blue} alt='Blue circle' />
+      <img className='w-[120px] h-[120px] absolute left-auto top-auto right-0 bottom-0 overflow-hidden object-cover align-middle md:w-[180px] md:h-[180px]' src={orange} alt='Orange circle' />
       <img className=' relative overflow-visible object-contain w-full h-full max-w-full align-middle' src={image} alt='Food and Drink' />
     </article>
   )
