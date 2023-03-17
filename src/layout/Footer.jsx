59c3fb021ffc179f1a39cad671e4ef3f768add8b
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-16 23:51:26 -0300

    feat: :iphone: add responsive styles to footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -8,35 +8,35 @@ import yellow from '../assets/images/yellowImage.jpg'
 const Footer = () => {
   return (
     <footer className=' bg-[#231656]'>
-      <div className=' flex flex-col-reverse mx-auto items-center justify-center lg:max-w-[1100px] w-[90%] relative py-[55px]'>
-        <div className=' flex items-center justify-center flex-col-reverse mx-auto lg:max-w-[1100px] w-full py-[35px] '>
+      <div className=' flex flex-col-reverse mx-auto items-center justify-center lg:max-w-[1100px] w-[90%] relative py-[55px] md:w-[85%] md:justify-between md:py-[25px]'>
+        <div className=' flex items-center justify-center flex-col-reverse mx-auto lg:max-w-[1100px] w-full py-[35px] md:w-[80%] md:max-w-[700px] md:items-start md:py-0'>
           <div className=' w-full flex flex-col md:max-w-[700px] items-start '>
-            <h2 className=' text-[40px] leading-[110%] text-white font-extrabold '>Stop spending time on your website and focus on your customers, with Appish.</h2>
-            <div className=' flex items-center justify-center mb-[30px] mt-5 mx-auto  '>
-              <a className='w-[45%] h-[60px] flex' href=''>
+            <h2 className=' text-[40px] leading-[110%] text-white font-extrabold md:text-[50px]'>Stop spending time on your website and focus on your customers, with Appish.</h2>
+            <div className=' flex items-center justify-center mb-[30px] mt-5 mx-auto md:mt-[50px] md:justify-between md:mx-0'>
+              <a className='w-[45%] h-[60px] flex md:w-[185px] md:h-[55px]' href=''>
                 <img className='object-contain align-middle' src={app} alt='' />
               </a>
-              <a className=' ml-[10px] w-[45%] h-[60px] flex ' href=''>
+              <a className=' ml-[10px] w-[45%] h-[60px] flex md:w-[185px] md:h-[55px] md:ml-[15px]' href=''>
                 <img className='object-contain align-middle' src={google} alt='' />
               </a>
             </div>
           </div>
         </div>
-        <div className=' w-[90%] '>
+        <div className=' w-[90%] md:w-[40%] md:mb-[60px]'>
           <img className=' align-middle w-full h-full' src={image} alt='' />
         </div>
-        <div className=' absolute top-[32%] right-[1%] w-[30px] h-[30px] left-auto bottom-auto overflow-hidden border-[1px] border-solid border-[#ececec] rounded-full'>
+        <div className=' absolute top-[32%] right-[1%] w-[30px] h-[30px] left-auto bottom-auto overflow-hidden border-[1px] border-solid border-[#ececec] rounded-full md:top-[17%] md:right-[19%] md:w-[51px] md:h-[51px]'>
           <img className='object-contain overflow-visible w-full h-full align-middle' src={pink} alt='' />
         </div>
-        <div className=' absolute top-[50%] left-auto right-0 bottom-auto w-[30px] h-[30px] overflow-hidden border-[1px] border-solid border-[#ececec] rounded-full'>
+        <div className=' absolute top-[50%] left-auto right-0 bottom-auto w-[30px] h-[30px] overflow-hidden border-[1px] border-solid border-[#ececec] rounded-full md:top-[46%] md:w-[60px] md:h-[60px]'>
           <img className='object-contain overflow-visible' src={yellow} alt='' />
         </div>
-        <div className=' absolute -right-[3%] -left-[1%] top-[15%] bottom-auto w-[30px] h-[30px] overflow-hidden border-[1px] border-solid border-[#ececec] rounded-full'>
+        <div className=' absolute -right-[3%] -left-[1%] top-[15%] bottom-auto w-[30px] h-[30px] overflow-hidden border-[1px] border-solid border-[#ececec] rounded-full md:left-0 md:top-[19%] md:right-auto md:w-[38px] md:h-[38px]'>
           <img className='object-contain overflow-visible' src={orange} alt='' />
         </div>
       </div>
-      <div className=' mt-[50px] pb-[30px] w-[90%] flex flex-col justify-between items-center lg:max-w-[1100px] mx-auto '>
-        <p className=' text-base leading-[26px] text-white'>
+      <div className=' mt-[50px] pb-[30px] w-[90%] flex flex-col justify-between items-center lg:max-w-[1100px] mx-auto md:pt-[60px] md:w-[85%] md:mt-0 '>
+        <p className=' text-base leading-[26px] text-white md:text-lg'>
           © All rights reserved.&nbsp;
           <a className=' text-white text-opacity-50' href=''>Conversasionflow</a>
           . Powered by&nbsp;
