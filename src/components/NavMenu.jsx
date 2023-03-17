174debf8de121d5bace7c152d520ce2919d34099
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-17 20:56:42 -0300

    feat: :iphone: add navmenu

---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,13 @@
+
+const NavMenu = () => {
+  return (
+    <div className='w-full flex flex-col items-end justify-center absolute bg-white'>
+      <a className=' w-full py-[15px] text-[16px] leading-[28px] text-[#231656] align-top px-[20px] mx-auto md:ml-5' href=''>Features</a>
+      <a className=' w-full py-[15px] text-[16px] leading-[28px] text-[#231656] align-top px-[20px] mx-auto md:ml-5' href=''>Why Appish</a>
+      <a className=' w-full py-[15px] text-[16px] leading-[28px] text-[#231656] align-top px-[20px] mx-auto md:ml-5' href=''>Testimonials</a>
+      <a className=' w-full py-[15px] text-[16px] leading-[28px] text-[#231656] align-top px-[20px] mx-auto md:ml-5' href=''>Awards</a>
+    </div>
+  )
+}
+
+export default NavMenu
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,43 @@
+import { useState } from 'react'
+import NavMenu from './NavMenu'
 import logo from '../assets/icons/appishLogo.svg'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <nav className=' pt-[15px]'>
-      <div className=' w-[90%] max-w-[1100px] mx-auto md:w-[85%]'>
-        <div className=' py-[10px] pl-5 flex flex-row w-full max-w-none rounded-[73px] mx-auto bottom-auto justify-between items-center'>
-          <a className=' w-[65px] h-[30px] md:w-[80px] ' href='#appish'>
-            <img className='  object-contain h-full align-middle' src={logo} alt='Appish' />
-          </a>
-          <div className=' flex flex-row justify-between items-center'>
-            <div className='hidden lg:block'>
-              <a className='py-5 text-[#231656] text-base leading-7 align-top px-5 text-left mx-auto' href='#features'>Features</a>
-              <a className='py-5 text-[#231656] text-base leading-7 align-top px-5 text-left mx-auto' href='#fast'>Why Appish</a>
-              <a className='py-5 text-[#231656] text-base leading-7 align-top px-5 text-left mx-auto' href='#testimonials'>Testimonials</a>
-              <a className='py-5 text-[#231656] text-base leading-7 align-top px-5 text-left mx-auto' href='#awards'>Awards</a>
-            </div>
-            <a className=' ml-[5px] py-[6px] px-[15px] rounded-full text-[13px] md:py-[10px] md:ml-[14px] md:px-[26px] md:text-[16px] bg-[#226cff] text-white leading-[28px] cursor-pointer font-semibold ' href='https://www.linkedin.com/in/enzo-ulloa-2639a1194/' target='_blank' rel='noreferrer'>Download Now</a>
-            <div className=' px-[10px] py-[5px] text-[#000] text-2xl cursor-pointer md:px-[18px] lg:hidden'>
-              <span>
-                <svg stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 512 512' height='24px' width='24px' xmlns='http://www.w3.org/2000/svg'><path fill='none' stroke-linecap='round' stroke-miterlimit='10' stroke-width='48' d='M88 152h336M88 256h336M88 360h336' /></svg>
-              </span>
+    <>
+      <nav className=' pt-[15px] relative'>
+        <div className=' w-[90%] max-w-[1100px] mx-auto md:w-[85%]'>
+          <div className=' py-[10px] pl-5 flex flex-row w-full max-w-none rounded-[73px] mx-auto bottom-auto justify-between items-center'>
+            <a className=' w-[65px] h-[30px] md:w-[80px] ' href='#appish'>
+              <img className='  object-contain h-full align-middle' src={logo} alt='Appish' />
+            </a>
+            <div className=' flex flex-row justify-between items-center'>
+              <div className='hidden lg:block'>
+                <a className='py-5 text-[#231656] text-base leading-7 align-top px-5 text-left mx-auto' href='#features'>Features</a>
+                <a className='py-5 text-[#231656] text-base leading-7 align-top px-5 text-left mx-auto' href='#fast'>Why Appish</a>
+                <a className='py-5 text-[#231656] text-base leading-7 align-top px-5 text-left mx-auto' href='#testimonials'>Testimonials</a>
+                <a className='py-5 text-[#231656] text-base leading-7 align-top px-5 text-left mx-auto' href='#awards'>Awards</a>
+              </div>
+              <a className=' ml-[5px] py-[6px] px-[15px] rounded-full text-[13px] md:py-[10px] md:ml-[14px] md:px-[26px] md:text-[16px] bg-[#226cff] text-white leading-[28px] cursor-pointer font-semibold ' href='https://www.linkedin.com/in/enzo-ulloa-2639a1194/' target='_blank' rel='noreferrer'>Download Now</a>
+              <button onClick={handleClick} className=' px-[10px] py-[5px] text-[#000] text-2xl cursor-pointer md:px-[18px] lg:hidden'>
+                <span>
+                  <svg stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 512 512' height='24px' width='24px' xmlns='http://www.w3.org/2000/svg'><path fill='none' stroke-linecap='round' stroke-miterlimit='10' stroke-width='48' d='M88 152h336M88 256h336M88 360h336' /></svg>
+                </span>
+              </button>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+        {
+          isOpen && <NavMenu />
+        }
+      </nav>
+    </>
   )
 }
 
