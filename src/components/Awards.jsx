3c20f107fdff6b4438b466a4a70c6c3bf6e84566
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-16 22:00:16 -0300

    feat: :iphone: add award section tablet responsive styles

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -4,7 +4,7 @@ import platinum from '../assets/images/platinum.png'
 const Awards = () => {
   return (
     <section className=' pt-[60px] '>
-      <div className=' pb-[100px] grid items-center gap-x-[70px] gap-y-[50px] grid-cols-1 relative max-w-[950px] w-[90%] mx-auto justify-between'>
+      <div className=' pb-[100px] grid items-center gap-x-[70px] gap-y-[50px] grid-cols-1 relative max-w-[950px] w-[90%] mx-auto justify-between md:pb-[150px] md:justify-center md:gap-y-[60px] md:w-[85%]'>
         <Award number='4.5/5' paragraph='On the iOS App Store.' />
         <Award number='4.8/5' paragraph='On the Android Play Store.' />
         <img className='max-w-[70%] w-[230px] mx-auto' src={platinum} alt='PLatinum nominee' />
