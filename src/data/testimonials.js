3fbbb13d3924cc09f2bfc432a867f9a3a6e893cc
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-20 19:50:21 -0300

    refactor: :recycle: refactor data

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -1,33 +1,38 @@
+import grey from '../assets/images/grey.jpg'
+import brown from '../assets/images/brown.jpg'
+import pink from '../assets/images/pink.jpg'
+import yellow from '../assets/images/yellowImage.jpg'
+import orange from '../assets/images/orange.jpg'
 
 const person1 = {
   name: 'Joe Cook',
   paragraph: 'Differentiate yourself from competitors with a website that makes personal finance look easy and builds trust in your app. Integrate with over 2000 tools easily using Webflow.',
   company: 'Product Designer at Uber.INC',
-  image: 'https://assets.website-files.com/604bbece9aeaba37cfb8d338/606e08180819094650f1d1fc_pexels-photo-2463236%201-min.jpg'
+  image: grey
 }
 const person2 = {
   name: 'Jessica Jobs',
   paragraph: 'Appish have been excellent development partners for quickSessions. Appish rapid development approach works in concert with our highly iterative process.',
   company: 'Product Designer at Uber.INC',
-  image: 'https://assets.website-files.com/604bbece9aeaba37cfb8d338/606e06bc1679474816b2380c_autumn-goodman-242825-unsplash-min.jpg'
+  image: yellow
 }
 const person3 = {
   name: 'Martin Luther',
   paragraph: 'After spending a lot of time researching online, I settled with Appish due to the reviews and the fact that their site claimed that conversion of excel spreadsheets to apps was a specialty. The team offered an innovative approach to mitigate communication barriers.',
   company: 'Product Designer at Uber.INC',
-  image: 'https://assets.website-files.com/604bbece9aeaba37cfb8d338/606e072ef0677cb10b332e39_autumn-goodman-242825-unsplash-2-min.jpg'
+  image: brown
 }
 const person4 = {
   name: 'Jenna Freud',
   paragraph: 'Appish continually went above and beyond the requirements of the project, developing a remarkably robust and functional platform. By handling any issues promptly, the team proved their excellent project management and communication skills.',
   company: 'Product Designer at Uber.INC',
-  image: 'https://assets.website-files.com/604bbece9aeaba37cfb8d338/606e072ffa58ae0c6fb98b71_autumn-goodman-242825-unsplash-min%20(1).jpg'
+  image: pink
 }
 const person5 = {
   name: 'David Issabell',
   paragraph: 'We needed to create an iOS app to calculate gas turbine performance and availability based on the user’s selection. Appish Technologies developed an iOS app with the required calculation functions.',
   company: 'Product Designer at Uber.INC',
-  image: 'https://assets.website-files.com/604bbece9aeaba37cfb8d338/606e072e856e7cf229c6e137_autumn-goodman-242825-unsplash-1-min.jpg'
+  image: orange
 }
 
 const testimonials = [
