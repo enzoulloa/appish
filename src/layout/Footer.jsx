ae1fe6d90bd29f887beffebc4629aa5c2d339df5
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-20 17:52:15 -0300

    feat: :sparkles: add mouse tracking animation

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react'
 import google from '../assets/icons/GooglePlay.svg'
 import app from '../assets/icons/AppStore.svg'
 import image from '../assets/images/Devicewithshadow.png'
@@ -6,6 +7,21 @@ import orange from '../assets/images/orange.jpg'
 import yellow from '../assets/images/yellowImage.jpg'
 
 const Footer = () => {
+  const pinkRef = useRef()
+  const orangeRef = useRef()
+  const yellowRef = useRef()
+
+  const handleMove = (e) => {
+    orangeRef.current.style.transform = `translate(${e.clientY / 50}px,${e.clientX / 50}px`
+    pinkRef.current.style.transform = `translate(${e.clientY / 50}px,${e.clientX / 50}px)`
+    yellowRef.current.style.transform = `translate(${e.clientY / 50}px,${e.clientX / 50}px)`
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousemove', handleMove)
+    return () => document.removeEventListener('mousemove', handleMove)
+  })
+
   return (
     <footer className=' bg-[#231656]'>
       <div className=' flex flex-col-reverse mx-auto items-center justify-center lg:max-w-[1100px] w-[90%] relative py-[55px] md:w-[85%] md:justify-between md:py-[25px] lg:py-[55px] lg:flex-row '>
@@ -25,14 +41,20 @@ const Footer = () => {
         <div className=' w-[90%] md:w-[40%] md:mb-[60px] lg:w-[25%]'>
           <img className=' align-middle w-full h-full' src={image} alt='' />
         </div>
-        <div className=' absolute top-[32%] right-[1%] w-[30px] h-[30px] left-auto bottom-auto overflow-hidden border-[1px] border-solid border-[#ececec] rounded-full md:top-[17%] md:right-[19%] md:w-[51px] md:h-[51px] lg:top-auto lg:-right-[8%] lg:bottom-[40%]'>
+        <div
+          ref={pinkRef} className=' absolute top-[32%] right-[1%] w-[30px] h-[30px] left-auto bottom-auto overflow-hidden border-[1px] border-solid border-[#ececec] rounded-full md:top-[17%] md:right-[19%] md:w-[51px] md:h-[51px] lg:top-auto lg:-right-[8%] lg:bottom-[40%]'
+        >
           <img className='object-contain overflow-visible w-full h-full align-middle' src={pink} alt='' />
         </div>
-        <div className=' absolute top-[50%] left-auto right-0 bottom-auto w-[30px] h-[30px] overflow-hidden border-[1px] border-solid border-[#ececec] rounded-full md:top-[46%] md:w-[60px] md:h-[60px] lg:top-auto lg:right-[33%] lg:bottom-[19%] lg:w-[78px] lg:h-[78px]'>
-          <img className='object-contain overflow-visible' src={yellow} alt='' />
+        <div
+          ref={yellowRef} className=' absolute top-[50%] left-auto right-0 bottom-auto w-[30px] h-[30px] overflow-hidden border-[1px] border-solid border-[#ececec] rounded-full md:top-[46%] md:w-[60px] md:h-[60px] lg:top-auto lg:right-[33%] lg:bottom-[19%] lg:w-[78px] lg:h-[78px]'
+        >
+          <img className='object-contain overflow-visible w-full h-full' src={yellow} alt='' />
         </div>
-        <div className=' absolute -right-[3%] -left-[1%] top-[15%] bottom-auto w-[30px] h-[30px] overflow-hidden border-[1px] border-solid border-[#ececec] rounded-full md:left-0 md:top-[19%] md:right-auto md:w-[38px] md:h-[38px] lg:left-auto lg:top-[16%] lg:right-[32%] lg:w-[38px] lg:h-[38px]'>
-          <img className='object-contain overflow-visible' src={orange} alt='' />
+        <div
+          ref={orangeRef} className=' absolute -right-[3%] -left-[1%] top-[15%] bottom-auto w-[30px] h-[30px] overflow-hidden border-[1px] border-solid border-[#ececec] rounded-full md:left-0 md:top-[19%] md:right-auto md:w-[38px] md:h-[38px] lg:left-auto lg:top-[16%] lg:right-[32%] lg:w-[38px] lg:h-[38px]'
+        >
+          <img className='object-contain overflow-visible w-full h-full' src={orange} alt='' />
         </div>
       </div>
       <div className=' mt-[50px] pb-[30px] w-[90%] flex flex-col justify-between items-center lg:max-w-[1100px] mx-auto md:pt-[60px] md:w-[85%] md:mt-0 lg:pt-[100px]'>
