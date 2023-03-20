4b73ed3274ff2abd42ab3891002f29733a4121b5
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-20 19:32:03 -0300

    fix: :bug: reorder header tags

--- src/components/Award.jsx
+++ src/components/Award.jsx
@@ -3,7 +3,7 @@ import star from '../assets/icons/stars.svg'
 const Award = ({ number, paragraph }) => {
   return (
     <article className=' flex items-center flex-col justify-between lg:items-start'>
-      <h5 className='text-[80px] text-[#231656] leading-[96px] font-extrabold md:text-[74px]'>{number}</h5>
+      <h3 className='text-[80px] text-[#231656] leading-[96px] font-extrabold md:text-[74px]'>{number}</h3>
       <img className='mb-[18px] align-middle ' src={star} alt='Rate' />
       <p className=' text-[17px] leading-6 font-normal text-[#6f6a80] md:leading-[140%]'>{paragraph}</p>
     </article>
--- src/components/Characteristic.jsx
+++ src/components/Characteristic.jsx
@@ -4,7 +4,7 @@ const Characteristic = ({ icon, title, paragraph, margin }) => {
     <article className={` flex flex-col justify-between items-start ${margin} md:justify-center md:flex-row lg:m-0`}>
       <img className=' mb-5 object-contain align-middle md:mt-[5px] md:mb-0' src={icon} alt='' />
       <div className='max-w-[85%] lg:max-w-[83%]'>
-        <h4 className=' text-xl leading-[29px] text-[#231656] font-semibold md:leading-[32px]'>{title}</h4>
+        <h3 className=' text-xl leading-[29px] text-[#231656] font-semibold md:leading-[32px]'>{title}</h3>
         <p className=' mt-[10px] text-base leading-[160%] text-[#6f6a80]'>{paragraph}</p>
       </div>
     </article>
